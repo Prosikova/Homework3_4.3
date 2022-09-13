7aad1f97895e6a7e291ee377f5c02bb57d42146c
Prosikova <lvenok182@mail.ru>
2022-09-13 03:10:53 +0300

    Решение задачи 3 в Домашней работе 3_4 (переменные часть2)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+byte class1 = 23;
+        byte class2 = 27;
+        byte class3 = 30;
+        int students = class1 + class2 + class3;
+        System.out.println("Всего учеников в трех классах " + students + " чел.");
+        short paper = 480;
+        int studentProvision = paper / students;
+        System.out.println("На каждого ученика расчитано " + studentProvision + " листов бумаги");
+            }
+}
